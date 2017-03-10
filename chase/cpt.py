65f02837a4aac32a55152b3cc38700404f60e73b
Doug Markant <dmarkant@gmail.com>
2017-03-10 09:38:33 +0100

    cleanup-up cpt

--- chase/cpt.py
+++ chase/cpt.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from copy import deepcopy
 
 def value_fnc(outcomes, pars):
     """Value weighting function
@@ -10,95 +10,33 @@ def value_fnc(outcomes, pars):
     w_loss: loss scaling parameter (default=1)
     """
     pow_gain = pars.get('pow_gain', 1.)
+    pow_loss = pars.get('pow_loss', pow_gain)
     w_loss   = pars.get('w_loss', 1.)
     gain     = (outcomes * (outcomes >= 0.)) ** pow_gain
-    loss     = -w_loss * ((-1 * (outcomes * (outcomes < 0.))) ** pow_gain)
+    loss     = -w_loss * ((-1 * (outcomes * (outcomes < 0.))) ** pow_loss)
     return gain + loss
 
-"""
-def pweight_prelec(option, pars):
-    prelec_elevation = pars.get('prelec_elevation', 1.)
-    prelec_gamma = pars.get('prelec_gamma', 1.)
-    prelec_elevation_loss = pars.get('prelec_elevation_loss', prelec_elevation)
-    prelec_gamma_loss = pars.get('prelec_gamma', prelec_gamma)
 
+def w(p, delta, gamma):
+    """Prelec decision weighting function
+
+    p:     array of probabilities
+    delta: elevation parameter
+    gamma: curvature parameter
+    """
+    assert np.sum(p < 0)==0 and np.sum(p > 1)==0
+    f = np.exp(-delta * ((-np.log(p)) ** gamma))
+
+    # truncate at [0, 1]
+    return np.clip(f, 0, 1)
+
+
+def rank_outcomes_by_domain(option):
+    """Create two pandas dataframes for
+    ranked outcomes, separated by gains/losses"""
     gaindf = None
     lossdf = None
-
-    def w(p):
-        return np.exp(-prelec_elevation * ((-np.log(p)) ** prelec_gamma))
-
-    def w_loss(p):
-        return np.exp(-prelec_elevation_loss * ((-np.log(p)) ** prelec_gamma_loss))
-
-    gains = []
-    losses = []
-
-    # separate gains and losses
-    for i, opt in enumerate(option):
-        if opt[0] > 0:
-            gains.append([i, opt[0], opt[1], np.nan])
-        elif opt[0] < 0:
-            losses.append([i, opt[0], opt[1], np.nan])
-
-    # if there are zero outcomes but no gains, group with losses
-    for i, opt in enumerate(option):
-        if opt[0] == 0:
-            if len(gains) == 0:
-                losses.append([i, opt[0], opt[1], np.nan])
-            else:
-                gains.append([i, opt[0], opt[1], np.nan])
-
-
-    if len(gains) > 0:
-        gaindf = pd.DataFrame(np.array(gains), columns=['id', 'outcome', 'pr', 'w']).sort('outcome').reset_index()
-
-        for i, row in gaindf.iterrows():
-
-            if i == (len(gaindf) - 1):
-                gaindf.ix[i, 'w'] = w(row['pr'])
-            else:
-                v = w(gaindf.iloc[i:]['pr'].sum()) - w(gaindf.iloc[(i+1):]['pr'].sum())
-                gaindf.ix[i,'w'] = v
-
-
-    if len(losses) > 0:
-        lossdf = pd.DataFrame(np.array(losses), columns=['id', 'outcome', 'pr', 'w']).sort('outcome').reset_index()
-
-        for i, row in lossdf.iterrows():
-            if i == 0:
-                lossdf.ix[i,'w'] = w_loss(row['pr'])
-            else:
-                lossdf.ix[i,'w'] = w_loss(lossdf.iloc[:(i+1)]['pr'].sum()) - w_loss(lossdf.iloc[:i]['pr'].sum())
-
-
-    weights = np.zeros(len(option))
-    for i in range(len(gains)):
-        weights[gaindf.iloc[i]['id']] = gaindf.iloc[i]['w']
-
-    for i in range(len(losses)):
-        weights[lossdf.iloc[i]['id']] = lossdf.iloc[i]['w']
-
-    try:
-        assert np.sum(np.isnan(weights))==0
-    except AssertionError:
-        print weights
-        print gaindf
-        print lossdf
-        print prelec_elevation
-        print prelec_gamma
-        print dummy
-
-    return weights
-"""
-
-
-
-def setup_option(option):
-    gaindf = None
-    lossdf = None
-
-    gains = []
+    gains  = []
     losses = []
 
     # separate gains and losses
@@ -124,143 +62,54 @@ def setup_option(option):
     return gaindf, lossdf
 
 
+
 def pweight_prelec(option, pars):
-
-    gaindf, lossdf = setup_option(option)
-
-    n_gains = gaindf.shape[0] if gaindf is not None else 0
-    n_losses = lossdf.shape[0] if lossdf is not None else 0
     prelec_elevation = pars.get('prelec_elevation', 1.)
     prelec_gamma = pars.get('prelec_gamma', 1.)
 
-    if gaindf is not None:
+    if 'gaindf' in option:
+        gaindf = deepcopy(option['gaindf'])
+        lossdf = deepcopy(option['lossdf'])
+    else:
+        gaindf, lossdf = rank_outcomes_by_domain(option)
+    n_gains = gaindf.shape[0] if gaindf is not None else 0
+    n_losses = lossdf.shape[0] if lossdf is not None else 0
 
+    if n_gains > 0:
         q = gaindf.pr.values
         r = np.append(np.cumsum(q[::-1])[::-1], [0])
         wr = w(r, prelec_elevation, prelec_gamma)
-        wr[0] = 1.
         wrd = -np.ediff1d(wr)
         gaindf.w = wrd
 
-    if lossdf is not None:
+    if n_losses > 0:
         q = lossdf.pr.values
         r = np.append([0], np.cumsum(q))
         wr = w(r, prelec_elevation, prelec_gamma)
         wrd = np.ediff1d(wr)
         lossdf.w = wrd
 
-
+    # put ranked weights back in original order
     weights = np.zeros(n_gains + n_losses)
     for i in range(n_gains):
         weights[gaindf.iloc[i]['id']] = gaindf.iloc[i]['w']
-
     for i in range(n_losses):
         weights[lossdf.iloc[i]['id']] = lossdf.iloc[i]['w']
 
-    try:
-        assert np.sum(np.isnan(weights))==0
-    except AssertionError:
-        print weights
-        print wr
-        print wrd
-        print gaindf
-        print lossdf
-        print prelec_elevation
-        print prelec_gamma
-        print dummy
+    assert np.sum(np.isnan(weights))==0
 
+    # normalize (in case of mixed option)
+    weights = weights / np.sum(weights)
 
     return weights
-
-
 
 
 def setup(problems):
     rdw = {}
     for pid in problems:
         options = problems[pid]
-
         rdw[pid] = []
-
         for option in options:
-
-            gaindf = None
-            lossdf = None
-
-            gains = []
-            losses = []
-
-            # separate gains and losses
-            for i, opt in enumerate(option):
-                if opt[0] > 0:
-                    gains.append([i, opt[0], opt[1], np.nan])
-                elif opt[0] < 0:
-                    losses.append([i, opt[0], opt[1], np.nan])
-
-            # if there are zero outcomes but no gains, group with losses
-            for i, opt in enumerate(option):
-                if opt[0] == 0:
-                    if len(gains) == 0:
-                        losses.append([i, opt[0], opt[1], np.nan])
-                    else:
-                        gains.append([i, opt[0], opt[1], np.nan])
-
-            if len(gains) > 0:
-                gaindf = pd.DataFrame(np.array(gains), columns=['id', 'outcome', 'pr', 'w']).sort('outcome').reset_index()
-            if len(losses) > 0:
-                lossdf = pd.DataFrame(np.array(losses), columns=['id', 'outcome', 'pr', 'w']).sort('outcome').reset_index()
-
+            gaindf, lossdf = rank_outcomes_by_domain(option)
             rdw[pid].append({'gaindf': gaindf, 'lossdf': lossdf})
-
     return rdw
-
-
-def w(p, delta, gamma):
-    return np.exp(-delta* ((-np.log(p)) ** gamma))
-
-
-def pweight_prelec_known_problem(option, pars):
-
-    gaindf = option['gaindf']
-    lossdf = option['lossdf']
-    n_gains = gaindf.shape[0] if gaindf is not None else 0
-    n_losses = lossdf.shape[0] if lossdf is not None else 0
-    prelec_elevation = pars.get('prelec_elevation', 1.)
-    prelec_gamma = pars.get('prelec_gamma', 1.)
-
-    if gaindf is not None:
-
-        q = gaindf.pr.values
-        r = np.append(np.cumsum(q[::-1])[::-1], [0])
-        wr = w(r, prelec_elevation, prelec_gamma)
-        wr[0] = 1.
-        wrd = -np.ediff1d(wr)
-        gaindf.w = wrd
-
-    if lossdf is not None:
-        q = lossdf.pr.values
-        r = np.append([0], np.cumsum(q))
-        wr = w(r, prelec_elevation, prelec_gamma)
-        wrd = np.ediff1d(wr)
-        lossdf.w = wrd
-
-
-    weights = np.zeros(n_gains + n_losses)
-    for i in range(n_gains):
-        weights[gaindf.iloc[i]['id']] = gaindf.iloc[i]['w']
-
-    for i in range(n_losses):
-        weights[lossdf.iloc[i]['id']] = lossdf.iloc[i]['w']
-
-    try:
-        assert np.sum(np.isnan(weights))==0
-    except AssertionError:
-        print weights
-        print gaindf
-        print lossdf
-        print prelec_elevation
-        print prelec_gamma
-        print dummy
-
-
-    return weights
