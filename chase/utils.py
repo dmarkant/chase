29b7a213a6bd3837920c0f23c545dee10397fb13
Doug Markant <dmarkant@gmail.com>
2017-03-10 14:56:10 +0100

    cleanup

--- chase/utils.py
+++ chase/utils.py
@@ -4,13 +4,23 @@ from copy import deepcopy
 
 
 def checkpath(dir):
+    """Recursively check that a path exists, and
+    create if not."""
     head, tail = os.path.split(dir)
     if head!="" and head!="~" and not os.path.exists(head): checkpath(head)
     if not os.path.exists(dir): os.mkdir(dir)
 
 
 def sim_id_str(name, fixed, par):
-    """Get a model label given fixed and fit parameter lists"""
+    """Get a model label given fixed and fit parameter lists.
+
+    Returns a string:
+        <name>(<list of free parameters>|<list of fixed parameters>)
+
+    name: identifier for simulation
+    fixed: fixed parameters
+    par: free parameters
+    """
     id = "%s(" % name
     if par!={}:
         keys = par.keys()
@@ -19,7 +29,7 @@ def sim_id_str(name, fixed, par):
             id += "%s,"%k
         id = id.rstrip(",")+"|"
 
-    if fixed!=None:
+    if fixed!={}:
         keys = fixed.keys()
         keys.sort()
         for k in keys:
@@ -41,6 +51,8 @@ def unpack(value, args):
 
 
 def outside_bounds(value, fitting):
+    """Given a list of parameters, check if any fall
+    outside bounds."""
     outside = False
     for v, p in zip(value, fitting):
         v_min, v_max = fitting[p][:2]
@@ -54,18 +66,41 @@ def bic(negllh, n_free, n_obs):
 
 
 def pfix(p):
-    return np.min([np.max([p, 1e-10]), 1])
+    return np.clip(p, 1e-5, 1 - 1e-5)
+    #c = 1e-5
+    #return np.min([np.max([p, c]), 1 - c])
 
 
-def pfixa(arr):
-    arr[arr < 1e-10] = 1e-10
-    return arr
+#def pfixa(arr):
+#    c = 1e-5
+#    arr[arr < c] = c
+#    arr[arr > (1-c)] = 1 - c
+#    return arr
 
 
-def pred_quantiles(p_stop, quantiles=[.25, .5, .75]):
-    return np.array([np.sum(np.cumsum(p_stop) < q) for q in quantiles]) + 1
+#def pred_quantiles(p_stop, quantiles=[.25, .5, .75]):
+#    return np.array([np.sum(np.cumsum(p_stop) <= q) for q in quantiles]) + 1
 
 
 def sample_from_discrete(cp):
     r = np.random.random()
     return np.where(r < np.cumsum(cp))[0][0]
+
+
+def expected_value(option):
+    return np.dot(option[:,0], option[:,1])
+
+
+def expected_variance(option):
+    values = option[:,0]
+    weights = option[:,1]
+    ev = expected_value(option)
+    return np.dot(weights, values ** 2) - ev ** 2
+
+
+#def expected_variance_mixture(option):
+#    values = option[:,0]
+#    weights = option[:,1]
+#    ev = expected_value(option)
+#    return np.dot(weights, values ** 2) - ev ** 2
+
